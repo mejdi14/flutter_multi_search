159a57d057ab0dc690a950eadeb11cdf3eb648fd
mejdi14 <mejdiclubiste@gmail.com>
2021-07-24 17:57:51 +0100

    here we go again

---
+++ example/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:example/SearchWidget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  var listSearch = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(child: Container()),
+              Container(
+                width: 40,
+                child: Icon(Icons.add),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
